8ab2f9fce47e8ef18c3e1dfe9764b5016e5bc3c3
Jay Ma <jma7889@gmail.com>
2015-11-21 16:29:05 -0500

    minor login change

--- js/components/login.js
+++ js/components/login.js
@@ -3,8 +3,13 @@ import React from 'react';
 const Login = React.createClass({ 
 
   render() {
-    return(<div>Welcome to login</div>);
-  }
+    return(
+    	<div>
+		<h3>Login</h3>
+    	Welcome to login
+    	</div>
+  )}
+
 });
 
 export default Login;  
