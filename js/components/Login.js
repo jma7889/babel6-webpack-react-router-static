1160568364acacce969cc5a66fb4a7242492f7a5
Jay Ma <jma7889@gmail.com>
2015-11-22 08:40:06 -0500

    add Login.js

---
+++ js/components/Login.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Login = React.createClass({ 
+
+  render() {
+    return(
+    	<div>
+		<h3>Login</h3>
+    	Welcome to login
+    	</div>
+  )}
+
+});
+
+export default Login;  
